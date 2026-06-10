f8936e380bb6b66137e602df635f0dd017995eb1
suog <104566003+5uog@users.noreply.github.com>
2026-06-11 01:32:02 +0900

    2026.06.11 01:31 (Update docs.policy.mjs)

--- tools/check_project/src/check/docs/docs.policy.mjs
+++ tools/check_project/src/check/docs/docs.policy.mjs
@@ -2,4 +2,4 @@
  * SPDX-FileCopyrightText: 2026 Kento Konishi
  * SPDX-License-Identifier: LicenseRef-All-Rights-Reserved
  */
-export const REQUIRED_README_TERMS = Object.freeze(['Ludoxel v3.6', 'LicenseRef-All-Rights-Reserved', 'NOTICE', 'third-party/', 'Copyright (c) 2026 Kento Konishi']);
+export const REQUIRED_README_TERMS = Object.freeze(['Ludoxel v3.6.0 法務情報', 'Copyright (c) 2026 小西拳斗.']);
