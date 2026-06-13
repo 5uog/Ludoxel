a3e3078cea083c4efff80d7190fd811f4850fd75
suog <104566003+5uog@users.noreply.github.com>
2026-06-14 03:14:51 +0900

    2026.06.14 03:14 (Update cloud_flow.py)

--- src/ludoxel/presentation/interface/settings/cloud_flow.py
+++ src/ludoxel/presentation/interface/settings/cloud_flow.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from ludoxel.application.preferences.cloud_flow import BACKEND_CLOUD_FLOW_DIRECTIONS, DEFAULT_BACKEND_CLOUD_FLOW_DIRECTION, normalize_backend_cloud_flow_direction
 
-_LABELS: dict[str, str] = {"east_to_west": "East -> West", "west_to_east": "West -> East", "south_to_north": "South -> North", "north_to_south": "North -> South"}
+_LABELS: dict[str, str] = {"east_to_west": "East to West", "west_to_east": "West to East", "south_to_north": "South to North", "north_to_south": "North to South"}
 
 CLOUD_FLOW_OPTIONS: tuple[tuple[str, str], ...] = tuple((value, _LABELS.get(str(value), str(value).replace("_", " ").title())) for value in BACKEND_CLOUD_FLOW_DIRECTIONS)
 
